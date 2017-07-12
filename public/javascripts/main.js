456fcdb24a909ce70cdb807bffa7cfee06d383e6
Tobi Adewuyi <tobi_adewuyi@yahoo.com>
2017-07-12 21:16:06 +0100

    charts hacked

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,71 +1,168 @@
-// /**
-//  * Created by Tobi on 7/10/2017.
-//  */
-// var table, mapping, chart;
-// anychart.onDocumentReady(function() {
-//
-//     table = anychart.data.table();
-//     table.addData([
-//         ['2015-12-24 12:00:00','511.53', '514.98', '505.79', '506.40'],
-//         ['2015-12-25 12:00:00','512.53', '514.88', '505.69', '507.34'],
-//         ['2015-12-26 12:00:00','511.83', '514.98', '505.59', '506.23'],
-//         ['2015-12-27 12:00:00','511.22', '515.30', '505.49', '506.47'],
-//         ['2015-12-28 12:00:00','510.35', '515.72', '505.23', '505.80'],
-//         ['2015-12-29 12:00:00','510.53', '515.86', '505.38', '508.25'],
-//         ['2015-12-30 12:00:00','511.43', '515.98', '505.66', '507.45'],
-//         ['2015-12-31 12:00:00','511.50', '515.33', '505.99', '507.98'],
-//         ['2016-01-01 12:00:00','511.32', '514.29', '505.99', '506.37'],
-//         ['2016-01-02 12:00:00','511.70', '514.87', '506.18', '506.75'],
-//         ['2016-01-03 12:00:00','512.30', '514.78', '505.87', '508.67'],
-//         ['2016-01-04 12:00:00','512.50', '514.77', '505.83', '508.35'],
-//         ['2016-01-05 12:00:00','511.53', '516.18', '505.91', '509.42'],
-//         ['2016-01-06 12:00:00','511.13', '516.01', '506.00', '509.26'],
-//         ['2016-01-07 12:00:00','510.93', '516.07', '506.00', '510.99'],
-//         ['2016-01-08 12:00:00','510.88', '515.93', '505.22', '509.95'],
-//         ['2016-01-09 12:00:00','509.12', '515.97', '505.15', '510.12'],
-//         ['2016-01-10 12:00:00','508.53', '516.13', '505.66', '510.42'],
-//         ['2016-01-11 12:00:00','508.90', '516.24', '505.73', '510.40']
-//     ]);
-//
-//     // mapping the data
-//     mapping = table.mapAs();
-//     mapping.addField('open', 1, 'first');
-//     mapping.addField('high', 2, 'max');
-//     mapping.addField('low', 3, 'min');
-//     mapping.addField('close', 4, 'last');
-//     mapping.addField('value', 4, 'last');
-//
-//     // defining the chart type
-//     chart = anychart.stock();
-//
-//     // set the series type
-//     chart.plot(0).ohlc(mapping).name('ACME Corp.');
-//
-//     // setting the chart title
-//     chart.title('AnyStock Basic Sample');
-//
-//     chart.container('container');
-//     chart.draw();
-// });
-$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
-    // Create the chart
-    Highcharts.stockChart('container', {
+jQuery(document).ready(() => {
+
+    let urlname = window.location.href;
+    const country = extractCountryFromURL(urlname);
+    let urlNew = 'https://ec2-35-177-19-196.eu-west-2.compute.amazonaws.com:3000/api/macrodata/' + country;
+
+    jQuery.getJSON(urlNew, (data) => {
+        let processedData = preProcessData(data);
+        // Create the chart
+        Highcharts.chart('container', {
+
+            /* rangeSelector: {
+             selected: 1
+             },*/
+
+            chart: {
+                zoomType: 'x'
+            },
 
 
-        rangeSelector: {
-            selected: 1
-        },
 
-        title: {
-            text: 'AAPL Stock Price'
-        },
+            title: {
+                text: 'Macroeconomic Data'
+            },
 
-        series: [{
-            name: 'AAPL',
-            data: data,
-            tooltip: {
-                valueDecimals: 2
-            }
-        }]
+            xAxis: [{
+                type: "datetime",
+                tickInterval: 24 * 3600 * 1000 * 365 // mills in a year.
+            }],
+            yAxis:[{
+                title: {
+                    text: 'GDP ($ Billion)',
+                    style: {
+                        color: Highcharts.getOptions().colors[0]
+                    }
+                },
+                labels:{
+                    // format: '${value} billion',
+                    style: {
+                        color: Highcharts.getOptions().colors[0]
+                    }
+                },
+                opposite: true
+            }, {
+                title: {
+                    text: 'GDP Per Capita ($)',
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                },
+                labels:{
+                    // format: '${value}',
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                },
+
+            },
+                {
+                    title: {
+                        text: 'Inflation Rate/Unemployment Rate (%)',
+                        style: {
+                            color: Highcharts.getOptions().colors[2]
+                        }
+                    },
+                    labels:{
+                        // format: '{value}%',
+                        style: {
+                            color: Highcharts.getOptions().colors[2]
+                        }
+                    },
+                    opposite: true
+
+                }],
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle'
+            },
+
+            series: [{
+                name: 'GDP',
+                data: processedData.gdp,
+                tooltip: {
+                    valueDecimals: 2,
+                    valuePrefix: '$'
+                }
+            },
+                {
+                    name: 'GDP Per Capita',
+                    data: processedData.gdpPerCapita,
+                    tooltip: {
+                        valueDecimals: 2,
+                        valuePrefix: '$'
+                    },
+                    yAxis: 1
+                },
+                {
+                    name: 'Inflation Rate',
+                    data: processedData.inflationRate,
+                    tooltip: {
+                        valueDecimals: 2,
+                        valueSuffix: '%'
+                    },
+                    color: Highcharts.getOptions().colors[2],
+                    shadow: true,
+                    yAxis: 2
+                },
+                {
+                    name: 'Unemployment Rate',
+                    data: processedData.unemploymentRate,
+                    tooltip: {
+                        valueDecimals: 2,
+                        valueSuffix: '%'
+                    },
+                    color: Highcharts.getOptions().colors[2],
+                    yAxis: 2
+                }]
+        });
     });
-})
+
+
+
+});
+
+function extractCountryFromURL(url) {
+    url = url.endsWith('/') ? url.slice(0, -1) : url;
+    let country = url.substr(url.lastIndexOf('/') + 1);
+    const separator = '-';
+    if (country.indexOf(separator) != -1){
+        let individualStrings = country.split(separator);
+        let capitalizedStrings = individualStrings.map((element) => element.charAt(0).toUpperCase() + element.slice(1));
+        country = capitalizedStrings.join("");
+    }
+    return country;
+}
+
+function preProcessData(dataPoints){
+    let gdpSeries = [];
+    let gdpPerCapitaSeries = [];
+    let inflationRateSeries = [];
+    let unemploymentRateSeries = [];
+
+    dataPoints.forEach((dataPoint) => {
+        let year = new Date(dataPoint['year'].toString()).getTime();
+        let gdp = parseFloat(dataPoint['gdp']);
+        let gdp_per_capita = parseFloat(dataPoint['gdp_per_capita']);
+        let inflation_rate = parseFloat(dataPoint['inflation_rate']);
+        let unemployment_rate = parseFloat(dataPoint['unemployment_rate']);
+
+        gdpSeries.push([year, gdp])
+        gdpPerCapitaSeries.push([year, gdp_per_capita]);
+        inflationRateSeries.push([year,inflation_rate]);
+        unemploymentRateSeries.push([year, unemployment_rate]);
+
+    });
+    return{
+        gdp: gdpSeries,
+        gdpPerCapita: gdpPerCapitaSeries,
+        inflationRate: inflationRateSeries,
+        unemploymentRate: unemploymentRateSeries
+    }
+
+
+
+}
+
+
